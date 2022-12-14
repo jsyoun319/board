a59b3d10bd6ceabd4396f35abd1309b04d0bff69
Jisun <Jsun319@naver.com>
2022-12-15 00:25:04 +0900

    Test Commit

---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,21 @@
+package com.study.board.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // 테이블을 의미
+@Data
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String content;
+}
